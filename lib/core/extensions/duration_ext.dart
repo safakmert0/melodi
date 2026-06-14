b858da6cc324943880a31391b880d45dc5571521
safakmert0 <safakmert6553@gmail.com>
2026-06-15 00:28:47 +0300

    Create missing duration_ext.dart extension

---
+++ lib/core/extensions/duration_ext.dart
@@ -0,0 +1,12 @@
+extension DurationExtension on Duration {
+  String toFormattedString() {
+    final hours = inHours;
+    final minutes = inMinutes.remainder(60);
+    final seconds = inSeconds.remainder(60);
+
+    if (hours > 0) {
+      return '$hours:${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
+    }
+    return '$minutes:${seconds.toString().padLeft(2, '0')}';
+  }
+}
